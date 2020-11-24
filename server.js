2096e1bccbca23e8e8518bed6308531b7ad9aeb7
Glitch (mater-bot-dev) <none>
2020-11-24 08:23:36 +0000

    💎👴 Updated with Glitch

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, config, token } = require("./config.json");
-const Enmap = require("enmap");
-const fs = require("fs");
-const express = require("express");
+const Enmap = require('enmap')
+const fs = require('fs');
+const express = require('express');
 const app = express();
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendStatus(200);
 });
 
 const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
 
 client.commands = new Enmap();
@@ -30,77 +30,70 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("m!pomoc - komendy", { type: "WATCHING" }); // good. check the bot works //Why are you messing with it lol
+  client.user.setActivity("m!pomoc", {type: 'WATCHING'}) // good. check the bot works //Why are you messing with it lol
 });
 
-client.on("message", (msg, message, channel) => {
-  if (msg.content === "/ping") {
-    //remember to change the prefix
-    msg
-      .reply("Ping?")
-      .then(m =>
-        m.edit(
-          `${msg.author}, Pong! Latency is ${m.createdTimestamp -
-            msg.createdTimestamp}ms. API Latency is ${Math.round(
-            client.ping
-          )} ms`
-        )
-      );
+client.on('message', async (msg, message, channel) => {
+  
+  if (msg.content === '/ping') { //remember to change the prefix
+  msg.reply('Ping?').then(m => m.edit(`${msg.author}, Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)} ms`))
   }
-
+  
   // if (msg.content === './<command>') {
-  // code here
-  // }
+ // code here
+// }
+      
+  
+  
+  
 });
 
 client.on("message", (message, args) => {
-  if (message.content.startsWith("/ban")) {
-    //remember to change the prefix
+    if (message.content.startsWith("/ban")) {//remember to change the prefix
 
-    // Easy way to get member object though mentions.
-    var member = message.mentions.members.first();
-    // ban
-    member
-      .ban()
-      .then(member => {
-        // Successmessage
-        message.channel.send(
-          ":wave: " +
-            member.displayName +
-            " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: "
-        );
-      })
-      .catch(error => {
-        // Failmessage
-        message.channel.send("Error:\n```fix\n" + error + "\n```");
-      });
-  }
-  if (message.content.startsWith("/kick")) {
-    //remember to change the prefix!
-    // Easy way to get member object though mentions.
-    var member = message.mentions.members.first();
-    // ban
-    member
-      .kick()
-      .then(member => {
-        // Successmessage
-        message.channel.send(
-          ":wave: " +
-            member.displayName +
-            " has been successfully kicked :point_right: "
-        );
-      })
-      .catch(error => {
-        // Failmessage
-        message.channel.send("Error:\n```fix\n" + error + "\n```");
-      });
-  }
+        // Easy way to get member object though mentions.
+        var member = message.mentions.members.first();
+        // ban
+        member.ban().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
+        }).catch((error) => {
+             // Failmessage
+            message.channel.send("Error:\n```fix\n" + error + "\n```");
+        });
+    }
+ if (message.content.startsWith("/kick")) { //remember to change the prefix!
+        // Easy way to get member object though mentions.
+        var member = message.mentions.members.first();
+        // ban
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch((error) => {
+             // Failmessage
+            message.channel.send("Error:\n```fix\n" + error + "\n```");
+        });
+    }
 }); //that is the right code why it not working
 
-//disable DM
 
+
+client.on("message", async message => {
+    
+  if (message.author.bot) return;
+  //disable DM
+  if (message.channel.dm === "dm") return;
+  
 //variables to work bot
+let messageArray = message.content.split(" ");
+let cmd = messageArray[0];
+let args = messageArray.slice(1);
+
+if (!message.content.startsWith(prefix)) return;
+let commandfile = client.commands.get(cmd.slice(prefix.length));
+if (commandfile) commandfile.run(client, message, args);
+});
 
 client.login(process.env.TOKEN);
